b3438f77f366b58c0bc851e922f65200a6419dce
PatraAyush <142981668+PatraAyush@users.noreply.github.com>
2024-06-27 13:46:20 +0530

    Update pattern.kt

--- pattern.kt
+++ pattern.kt
@@ -1,1 +1,22 @@
-
+fun main() {
+    var i=0
+    var j=0
+    for(i in 0 until 5){
+        for(j in 0 until (5-i-1)){
+            print(" ")
+        }
+        for(j in 0 until (2*i+1)){
+            print("*")
+        }
+        println()
+    }
+    for(i in 5 downTo 0){
+        for(j in 0 until (5-i)){
+            print(" ")
+        }
+        for(j in 0 until (2*i-1)){
+            print("*")
+        }
+        println()
+    }
+}
