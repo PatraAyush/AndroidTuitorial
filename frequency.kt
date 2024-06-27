0cfc3059a2efd13e259de7d235903783b90b18f6
PatraAyush <142981668+PatraAyush@users.noreply.github.com>
2024-06-27 14:31:45 +0530

    Create frequency.kt

---
+++ frequency.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val str = "My name is Ayush Patra."
+    val ch = 'a'
+    var fr = 0
+
+    for (i in str) {
+        if (i == ch) {
+            fr++
+        }
+    }
+    println(fr)
+}
